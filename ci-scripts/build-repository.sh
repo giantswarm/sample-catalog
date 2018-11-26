a12d00812607a3fe378055d973d955c726c7e35d
Tobias Richter <tr4711@gmail.com>
2018-11-26 19:30:23 +0100

    Make linter happy

--- ci-scripts/build-repository.sh
+++ ci-scripts/build-repository.sh
@@ -15,11 +15,11 @@ readonly HELM_REPO_URL=https://giantswarm.github.com/${REPONAME}
 main() {
     setup_helm_client
 
-    if [ ! download_latest_charts ]; then
+    if ! download_latest_charts; then
         log_error "Not all charts could be downloaded!"
     fi
 
-    if [ ! sync_repo "${HELM_REPO_URL}" ]; then
+    if ! sync_repo "${HELM_REPO_URL}"; then
         log_error "Not all charts could be packaged and synced!"
     fi
 
@@ -45,18 +45,18 @@ setup_helm_client() {
 download_latest_charts() {
   while IFS="" read -r app || [ -n "${app}" ]
   do
-      local url=$(curl -s https://api.github.com/repos/${app}/releases/latest | jq -r .assets[0].browser_download_url)
-      local chart=$(echo ${url} | tr "/" " " | awk '{print $NF}')
+      release_url=$(curl -s "https://api.github.com/repos/${app}/releases/latest" | jq -r .assets[0].browser_download_url)
+      chart=$(echo "${release_url}" | tr "/" " " | awk '{print $NF}')
 
       # Check if release exists and not already present
-      if [ "${url}" == "null" ];then
+      if [ "${release_url}" == "null" ];then
         echo "No GitHub release of '${app}' found!"
         exit 1
       elif [ -e "${chart}" ];then
         echo "${chart} already present, skipping!"
       else
         echo "Downloading chart '${chart}'..."
-        wget -q -P sync ${url}
+        wget -q -P sync "${release_url}"
       fi
   done < ${APPS_FILE}
 }
@@ -73,11 +73,11 @@ sync_repo() {
     if helm repo index --url "${repo_url}" --merge "index.yaml" "sync"; then
         mv -f ./sync/* .
 
-        git add *.tgz
+        git add ./*.tgz
         git add index.yaml
 
         git commit -m "Auto build ${REPONAME}" --author="Taylor Bot <dev@giantswarm.io>"
-        git push -q https://${PERSONAL_ACCESS_TOKEN}@github.com/giantswarm/${REPONAME}.git master
+        git push -q "https://${PERSONAL_ACCESS_TOKEN}@github.com/giantswarm/${REPONAME}.git" master
     else
         log_error "Exiting because unable to update index. Not safe to push update."
         exit 1
