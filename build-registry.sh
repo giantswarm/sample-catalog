7394d06e81a07e70f8dbb81eb773c5ed0f5271fd
Tobias Richter <tr4711@gmail.com>
2018-11-26 18:14:16 +0100

    Unify variable references

--- build-registry.sh
+++ build-registry.sh
@@ -9,17 +9,17 @@ readonly PERSONAL_ACCESS_TOKEN=$2
 
 readonly APPS_FILE=./APPS
 readonly HELM_URL=https://storage.googleapis.com/kubernetes-helm
-readonly HELM_TARBALL=helm-v2.9.1-linux-amd64.tar.gz
+readonly HELM_TARBALL=helm-v2.11.0-linux-amd64.tar.gz
 readonly HELM_REPO_URL=https://giantswarm.github.com/${REPONAME}
 
 main() {
     setup_helm_client
 
-    if ! download_latest_charts; then
+    if [ ! download_latest_charts ]; then
         log_error "Not all charts could be downloaded!"
     fi
 
-    if ! sync_repo "$HELM_REPO_URL"; then
+    if [ ! sync_repo "${HELM_REPO_URL}" ]; then
         log_error "Not all charts could be packaged and synced!"
     fi
 
@@ -33,29 +33,29 @@ cleanup() {
 setup_helm_client() {
     echo "Setting up Helm client..."
 
-    curl --user-agent curl-ci-sync -sSL -o "$HELM_TARBALL" "$HELM_URL/$HELM_TARBALL"
-    tar xzfv "$HELM_TARBALL"
+    curl --user-agent curl-ci-sync -sSL -o "${HELM_TARBALL}" "${HELM_URL}/${HELM_TARBALL}"
+    tar xzfv "${HELM_TARBALL}"
 
     PATH="$(pwd)/linux-amd64/:$PATH"
 
     helm init --client-only
-    helm repo add "$REPONAME" "$HELM_REPO_URL"
+    helm repo add "${REPONAME}" "${HELM_REPO_URL}"
 }
 
 download_latest_charts() {
-  while IFS="" read -r app || [ -n "$app" ]
+  while IFS="" read -r app || [ -n "${app}" ]
   do
       local url=$(curl -s https://api.github.com/repos/${app}/releases/latest | jq -r .assets[0].browser_download_url)
       local chart=$(echo ${url} | tr "/" " " | awk '{print $NF}')
 
       # Check if release exists and not already present
       if [ "${url}" == "null" ];then
-        echo "No GitHub release of '$app' found!"
+        echo "No GitHub release of '${app}' found!"
         exit 1
       elif [ -e "${chart}" ];then
         echo "${chart} already present, skipping!"
       else
-        echo "Downloading chart '$chart'..."
+        echo "Downloading chart '${chart}'..."
         wget -q -P sync ${url}
       fi
   done < ${APPS_FILE}
@@ -64,19 +64,19 @@ download_latest_charts() {
 sync_repo() {
     local repo_url="${1?Specify repo url}"
 
-    if [ -d "sync" ]; then
+    if [ ! -d "sync" ]; then
       echo "No new releases found. Terminating"
       return 0
     fi
 
     echo "Syncing repo..."
-    if helm repo index --url "$repo_url" --merge "index.yaml" "sync"; then
+    if helm repo index --url "${repo_url}" --merge "index.yaml" "sync"; then
         mv -f ./sync/* .
 
         git add *.tgz
         git add index.yaml
 
-        git commit -m "Auto build ${REPONAME}" --author="Taylor Bot dev@giantswarm.io"
+        git commit -m "Auto build ${REPONAME}" --author="Taylor Bot <dev@giantswarm.io>"
         git push -q https://${PERSONAL_ACCESS_TOKEN}@github.com/giantswarm/${REPONAME}.git master
     else
         log_error "Exiting because unable to update index. Not safe to push update."
